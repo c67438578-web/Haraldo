1d2ab0fd7ef70d697fbcb099115790abe30ec3f6
c67438578-web <c67438578@gmail.com>
2025-12-26 06:57:33 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", 
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,ts,jsx,tsx}',       // Pega tudo do app
+    './components/**/*.{js,ts,jsx,tsx}', // Pega tudo dos componentes
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
