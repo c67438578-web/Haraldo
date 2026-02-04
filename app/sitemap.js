378565227f9fe621c9054bcae9faf3667e0473a7
c67438578-web <c67438578@gmail.com>
2026-02-04 15:43:02 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,9 +1,68 @@
-export default function sitemap() {
-  return [
-    {
-      url: 'https://orlandoaircond.com',
-      lastModified: new Date(),
-    },
-    // Adicione outras rotas aqui
-  ]
-}
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  
+  <!-- Homepage -->
+  <url>
+    <loc>https://orlandoaircond.com/</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  
+  <!-- Services -->
+  <url>
+    <loc>https://orlandoaircond.com/services</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  
+  <!-- AC Repair -->
+  <url>
+    <loc>https://orlandoaircond.com/ac-repair</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  
+  <!-- AC Installation -->
+  <url>
+    <loc>https://orlandoaircond.com/ac-installation</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  
+  <!-- AC Maintenance -->
+  <url>
+    <loc>https://orlandoaircond.com/ac-maintenance</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  
+  <!-- About -->
+  <url>
+    <loc>https://orlandoaircond.com/about</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  
+  <!-- Contact -->
+  <url>
+    <loc>https://orlandoaircond.com/contact</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  
+  <!-- Emergency Service -->
+  <url>
+    <loc>https://orlandoaircond.com/emergency-ac-repair</loc>
+    <lastmod>2026-02-04</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+
+</urlset>
